1befc398cd250dd92e134f7a08449929622cf484
yutsukushi <ykushibi@gmail.com>
2019-07-02 23:07:10 -0400

    Functioning LIRI Bot CLI, still need to update README

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
+var moment = require('moment');
 
 
 // declaring var to user input on index 2.
@@ -13,28 +14,30 @@ var action = process.argv[2]; //user input of which command they want to use
 var input = process.argv[3]; //user input of which artist they want to look up
 
 // switch-case statement for user input of multiple commands:
-switch (action) {
-    case "concert-this":
-        band();
-        break;
+function startProg(action, input){
+    switch (action) {
+        case "concert-this":
+            band(input);
+            break;
+    
+        case "spotify-this-song":
+            spot(input);
+            break;
+    
+        case "movie-this":
+            movie(input);
+            break;
+    
+        case "do-what-it-says":
+            runspot();
+            break;
+        default: console.log("choose the right option");
+    }
 
-    case "spotify-this-song":
-        spot();
-        break;
-
-    case "movie-this":
-        movie();
-        break;
-
-    case "do-what-it-says":
-        runspot();
-        break;
-    default: console.log("choose the right option");
 }
+startProg(action,input);
 
-// call function for API Bands in Town to retrieve artist
-// print called information
-function band() {
+function band(input) {
     var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
     
     axios
@@ -49,13 +52,18 @@ function band() {
             console.log("===============================================================");
             console.log("Venue: " + show.venue.name);
             console.log("Location: " + show.venue.country);
-            console.log("Show times: " + show.datetime);
+            console.log("Show times: " + moment(show.datetime).format("MMM Do YY"));
 
         }
     })
 }
 
-function spot() {
+function spot(input) {
+    if (input === undefined) {
+
+        input = "The Sign by Ace of Base";
+
+    }
 
     spotify.search({ type: 'track', query: input, limit: 5 }, function(err, response) {
         if (err) {
@@ -64,7 +72,7 @@ function spot() {
        
         console.log("Song Results");
 
-        if (input !== undefined) {
+       
             for (var i = 0; i < response.tracks.items.length; i++) {
 
                 var songinfo = response.tracks.items[i];
@@ -73,57 +81,39 @@ function spot() {
                 console.log("Artist: " + songinfo.album.artists[0].name); 
                 console.log("Song: " + songinfo.name);
                 console.log("Song Preview: " + songinfo.preview_url);
-                console.log("Song Album: " + JSON.stringify(songinfo.album.external_urls))
+                console.log("Song Album: " + songinfo.album.external_urls.spotify);
                 
-        }} else {
-
-            // TODO: else statement to run default code for "The Sign of Ace of Base"
-            
-            // spotify.search({ type: 'track', query: "The Sign by Ace of Base", limit: 5 }, function(err, response) {
-            //     if (err) {
-            //       return console.log('Error occurred: ' + err);
-            //     }
-
-            // })
-            
-        }
+            }
     });
       
 }
 
- function movie() {
+ function movie(input) {
+    if (input === undefined) {
+
+        input= "Mr. Nobody";
+
+    }
+
      var queryURL = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
      axios
      .get(queryURL)
-     .then(function(res) {
+     .then(function( res) {
         
-        // console.log(res.data);
-        // if (input !== undefined) {
+        var movieinfo = res.data;
+        console.log("You search for: " + input);
+        console.log("=======================================")
+        console.log("Movie title: " + movieinfo.Title); 
+        console.log("Release year: " + movieinfo.Released);
+        console.log("imDb Rating: " + movieinfo.imdbRating); 
+        console.log("Rotten Tomatoes Rating: " +movieinfo.Ratings[1].Source + " value: " + movieinfo.Ratings[1].Value);
+        console.log("Made in: " + movieinfo.Country); 
+        console.log("Language: " + movieinfo.Language); 
+        console.log("Short Plot: " + movieinfo.Plot); 
+        console.log("Actors in the Movie: " + movieinfo.Actors);
 
-                var movieinfo = res.data;
-                console.log("You search for: " + input);
-                console.log("=======================================")
-                console.log("Movie title: " + movieinfo.Title); 
-                console.log("Release year: " + movieinfo.Released);
-                console.log("imDb Rating: " + movieinfo.imdbRating); 
-                console.log("Rotten Tomatoes Rating: " + JSON.stringify(movieinfo.Ratings[1])); 
-                console.log("Made in: " + 
-                    movieinfo.Country); 
-                console.log("Language: " + movieinfo.Language); 
-                console.log("Short Plot: " + movieinfo.Plot); 
-                console.log("Actors in the Movie: " + movieinfo.Actors);
-
-        // } else {
-        //     // TODO: run Mr. Nobody
-        // }  
-
-     }), function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-        
-    }
+     })
     
  }
 
@@ -134,6 +124,10 @@ function runspot() {
         }
 
         console.log(data);
+        var dataArr = data.split(",");
+        var action = dataArr[0];
+        var secondParam = dataArr[1];
+        startProg(action, secondParam);
     })
 }
    
